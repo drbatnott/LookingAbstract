87e2180222c0e192a4b4c8c4fdb70e37a9fcd1e9
Peter Blanchfield <zygomy@gmail.com>
2021-11-29 15:56:08 +0000

    Built a test for implementing the abstract class Overriding the abstract method Overloading the other method

---
+++ AbstractTest/Bird.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractTest
+{
+    /// <summary>
+    /// Bird is an abstract class
+    /// When we want to use it we will need to implement the abstract method birdCall
+    /// but we will be able to use the non abstract method birdnest
+    /// </summary>
+    abstract class Bird
+    {
+        public abstract void birdCall();// birdCall is an abstract method
+        public void birdNest()
+        {
+            Console.WriteLine("zzzzzzz");
+        }
+    }
+}
---
+++ AbstractTest/Penguin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractTest
+{
+    class Penguin : Bird
+    {
+        float m_volume; // this variable has class scope or can be called a member variable 
+        public override void birdCall()
+        {
+            m_volume = 27.3f;
+            float lVolume = 2 * m_volume;
+            //the local volume lVolume has only got method scope
+            Console.WriteLine("Squuuuuaaaaakkkkkkkkkk at " + lVolume) ;
+            //throw new NotImplementedException(); This was put in by VS but not needed
+        }
+        //birdNest(float f) overloads the birdNest method
+        public void birdNest(float f)// it has a different signature to the root
+        {
+            Console.WriteLine("zzzzzzzz at " + f);
+        }
+    }
+}
